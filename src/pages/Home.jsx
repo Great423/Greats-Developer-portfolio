3b39658e3a9910777ed2c4762fe8408ca139becf
Great423 <chukwukaonyenokwe@gmail.com>
2024-04-27 11:37:32 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Skills = lazy(() => import('../components/Skills'));
 const Services = lazy(() => import('../components/Services'));
 const Work = lazy(() => import('../components/Work'));
 
-// Non-lazy-loaded components (usually smaller ones)
+// Non-lazy-loaded components
 import Nav from '../components/Nav';
 import Banner from '../components/Banner';
 const Home = () => {
